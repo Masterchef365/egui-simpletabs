81a7e19f6ace43d8112f1d6695f9568b1242e8d1
Masterchef365 <duncan.freeman1@gmail.com>
2026-03-15 03:30:17 -0700

    More buttons!

--- src/lib.rs
+++ src/lib.rs
@@ -145,6 +145,11 @@ pub fn play_pause_button(ui: &mut Ui, paused: &mut bool) {
         false => "   ⏸   ",
     };
 
+    let hover_text = match *paused {
+        true => "Play",
+        false => "Pause",
+    };
+
     let color = match *paused {
         true => Color32::DARK_RED,
         false => Color32::from_rgb(79, 200, 0),
@@ -154,17 +159,28 @@ pub fn play_pause_button(ui: &mut Ui, paused: &mut bool) {
 
     let button = egui::Button::new(text)
         .fill(color)
-        .min_size(egui::Vec2::new(0.0, 40.0));
+        .min_size(BUTTON_SIZE);
 
-    if ui.add(button).clicked() {
+    let resp = ui.add(button).on_hover_text(hover_text);
+    if resp.clicked() {
         *paused = !*paused;
     }
 }
 
-pub fn single_step_button(ui: &mut Ui) -> egui::Response {
-    let text = RichText::new("   ⏭   ").color(Color32::WHITE);
+const BUTTON_SIZE: egui::Vec2 = egui::Vec2::splat(40.0);
 
-    let button = egui::Button::new(text).min_size(egui::Vec2::new(0.0, 40.0));
+pub fn single_step_button(ui: &mut Ui) -> egui::Response {
+    serious_button(ui, "⏭").on_hover_text("Single-step")
+}
+
+pub fn reset_step_button(ui: &mut Ui) -> egui::Response {
+    serious_button(ui, "↺").on_hover_text("Reset")
+}
+
+pub fn serious_button(ui: &mut Ui, symbol: &str) -> egui::Response {
+    let text = RichText::new(format!("   {symbol}   ")).color(Color32::WHITE);
+
+    let button = egui::Button::new(text).min_size(BUTTON_SIZE);
 
     ui.add(button)
 }
